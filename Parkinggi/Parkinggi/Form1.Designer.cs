fb1778ebd9508893a1edda70908ace33a95ecf7b
Lukasz <lukasz.wpvp@gmail.com>
2017-01-23 21:33:38 +0100

    "first commit"

---
+++ Parkinggi/Parkinggi/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Parkinggi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ibOriginal = new Emgu.CV.UI.ImageBox();
+            this.ibProcessed = new Emgu.CV.UI.ImageBox();
+            this.btnPouseOrResume = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ibProcessedTaken = new Emgu.CV.UI.ImageBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ibOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ibProcessed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ibProcessedTaken)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ibOriginal
+            // 
+            this.ibOriginal.Location = new System.Drawing.Point(13, 13);
+            this.ibOriginal.Name = "ibOriginal";
+            this.ibOriginal.Size = new System.Drawing.Size(640, 480);
+            this.ibOriginal.TabIndex = 2;
+            this.ibOriginal.TabStop = false;
+            // 
+            // ibProcessed
+            // 
+            this.ibProcessed.Location = new System.Drawing.Point(659, 13);
+            this.ibProcessed.Name = "ibProcessed";
+            this.ibProcessed.Size = new System.Drawing.Size(160, 160);
+            this.ibProcessed.TabIndex = 2;
+            this.ibProcessed.TabStop = false;
+            // 
+            // btnPouseOrResume
+            // 
+            this.btnPouseOrResume.Location = new System.Drawing.Point(761, 412);
+            this.btnPouseOrResume.Name = "btnPouseOrResume";
+            this.btnPouseOrResume.Size = new System.Drawing.Size(124, 63);
+            this.btnPouseOrResume.TabIndex = 3;
+            this.btnPouseOrResume.Text = "wczytaj";
+            this.btnPouseOrResume.UseVisualStyleBackColor = true;
+            this.btnPouseOrResume.Click += new System.EventHandler(this.btnPouseOrResume_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(678, 336);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(678, 363);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(675, 396);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // ibProcessedTaken
+            // 
+            this.ibProcessedTaken.Location = new System.Drawing.Point(825, 13);
+            this.ibProcessedTaken.Name = "ibProcessedTaken";
+            this.ibProcessedTaken.Size = new System.Drawing.Size(160, 160);
+            this.ibProcessedTaken.TabIndex = 7;
+            this.ibProcessedTaken.TabStop = false;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(845, 363);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 8;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(825, 179);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(160, 160);
+            this.imageBox1.TabIndex = 9;
+            this.imageBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(997, 505);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.ibProcessedTaken);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnPouseOrResume);
+            this.Controls.Add(this.ibProcessed);
+            this.Controls.Add(this.ibOriginal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ibOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ibProcessed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ibProcessedTaken)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox ibOriginal;
+        private Emgu.CV.UI.ImageBox ibProcessed;
+        private System.Windows.Forms.Button btnPouseOrResume;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private Emgu.CV.UI.ImageBox ibProcessedTaken;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private Emgu.CV.UI.ImageBox imageBox1;
+    }
+}
+
---
+++ Parkinggi/Parkinggi/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Windows.Forms;
+using Emgu.CV;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace Parkinggi
+{
+    public partial class Form1 : Form
+    {
+        Mat img, img1;
+        List<Mat> spacesPattern, spacesActual;
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var a = (ComboBox)sender;
+            ibProcessedTaken.Image = spacesActual[a.SelectedIndex];
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var a = (ComboBox)sender;
+            ibProcessed.Image = spacesPattern[a.SelectedIndex];
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var a = (ComboBox)sender;
+            if(a.SelectedItem.Equals(1))
+            {
+                ibOriginal.Image = img;
+            }
+            else
+            {
+                ibOriginal.Image = img1;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_FormClodes(object sender, FormClosedEventArgs e)
+        {
+        }
+
+        private void btnPouseOrResume_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add(1);
+            comboBox1.Items.Add(2);
+            
+            img = new Mat();
+            img1 = new Mat();
+            img = CvInvoke.Imread("E:/parkFree.png", Emgu.CV.CvEnum.LoadImageType.Grayscale);
+            ibOriginal.Image = img;
+            img1 = CvInvoke.Imread("E:/park.png", Emgu.CV.CvEnum.LoadImageType.Grayscale);
+
+            spacesPattern = new List<Mat>();
+            spacesActual = new List<Mat>();
+
+            for(int i=0; i<4; i++)
+            {
+                spacesPattern.Add(new Mat(img, new Rectangle(0 + 160 * i, 0, 160, 160)));
+                spacesActual.Add(new Mat(img1, new Rectangle(0 + 160 * i, 0, 160, 160)));
+                spacesPattern.Add(new Mat(img, new Rectangle(0 + 160 * i, 320, 160, 160)));
+                spacesActual.Add(new Mat(img1, new Rectangle(0 + 160 * i, 320, 160, 160)));
+            }
+
+            label1.Text = "";
+
+            var freeSpaces = spacesPattern.Count;
+            var takenSpaces = 0;
+
+            for(int i=0; i<spacesPattern.Count; i++)
+            {
+                comboBox2.Items.Add(i);
+                comboBox3.Items.Add(i);
+                if(!spacesPattern[i].Equals(spacesActual[i]))
+                {
+                    takenSpaces++;
+                }
+            }
+            //var adsff = spacesPattern[3] - spacesActual[3];
+            var asdf = new Mat();
+
+            CvInvoke.AbsDiff(spacesPattern[3], spacesActual[3], asdf);
+
+            imageBox1.Image = asdf;
+
+            label1.Text = $"Wolne miejsca: {freeSpaces-takenSpaces}; Zajęte miejsca: {takenSpaces};";
+
+            ibProcessed.Image = spacesPattern[0];
+            ibProcessedTaken.Image = spacesActual[0];
+        }
+    }
+}
